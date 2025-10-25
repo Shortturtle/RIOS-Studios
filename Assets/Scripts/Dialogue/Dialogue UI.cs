ea4c2ed0163f212ae4153536b573ba4f2fdf8184
Esquire5100 <KLEAH.24@ichat.sp.edu.sg>
2025-10-25 11:32:01 +0800

    Closes dialoguebox when done

--- Assets/Scripts/Dialogue/Dialogue UI.cs
+++ Assets/Scripts/Dialogue/Dialogue UI.cs
@@ -4,6 +4,7 @@ using System.Collections;
 
 public class DialogueUI : MonoBehaviour
 {
+    [SerializeField] private GameObject dialogueBox;
     [SerializeField] private TMP_Text textLabel;
     [SerializeField] private DialogueObject testDialogue;
 
@@ -12,11 +13,13 @@ public class DialogueUI : MonoBehaviour
     private void Start()
     {
         typewriterEffect=GetComponent<TypewriterEffect>();
+        CloseDialogueBox();
         ShowDialogue(testDialogue);
     }
 
     public void ShowDialogue(DialogueObject dialogueObject)
     {
+        dialogueBox.SetActive(true);
         StartCoroutine(RunDialogue(dialogueObject));
     }
 
@@ -27,5 +30,13 @@ public class DialogueUI : MonoBehaviour
             yield return typewriterEffect.Run(dialogue, textLabel);
             yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)); //"||" means "or"
         }
+
+        CloseDialogueBox();
+    }
+
+    private void CloseDialogueBox()
+    {
+        dialogueBox.SetActive(false);
+        textLabel.text = string.Empty;
     }
 }
