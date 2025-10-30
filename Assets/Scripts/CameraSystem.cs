4ce4faa572cb9e9694de2bd3547fe0cf01c0e2e3
beanedhuman <zpguo2007@gmail.com>
2025-10-30 22:39:21 +0800

    Concept 2 camera moves (BASIC)
    the base movement for it is done
    ill continue working on it later
    not now
    im lazy

--- Assets/Scripts/CameraSystem.cs
+++ Assets/Scripts/CameraSystem.cs
@@ -8,11 +8,12 @@ public class CameraSystem : MonoBehaviour
     private CharacterController characterController;
     public GameObject pauseScreen;
 
-    public float moveSpeed;
+    public float moveSpeed = 10f;
     public float speed = 5f;
-    public float turnRate = 360f;
+    //public float turnRate = 360f;
 
-    float moveDirection = 0f, turnDirection = 0f;
+    float moveForwardDirection = 0f;
+    float moveSideDirection = 0f;
 
     private void Awake()
     {
@@ -30,18 +31,43 @@ public class CameraSystem : MonoBehaviour
 
         Vector3 moveDir = new Vector3(0, 0, 0);
 
+        if(moveForwardDirection > 0)
+        {
+            Debug.Log("mfd >0");
+            moveDir.z = +1f;
+        }
+        if (moveForwardDirection < 0)
+        {
+            Debug.Log("mfd <0");
+            moveDir.z = -1f;
+        }
+        if (moveForwardDirection == 0) { moveDir.z = 0f; }
 
+        if (moveSideDirection > 0) { moveDir.x = +1f; }
+        if (moveSideDirection < 0) { moveDir.x = -1f; }
+        if (moveSideDirection == 0) { moveDir.x = 0f; }
 
         transform.position += moveDir * moveSpeed * Time.deltaTime;
     }
-    //public void HorizontalMove(InputAction.CallbackContext ctx)
-    //{
-    //    Move(ctx.ReadValue<float>());
-    //}
-    //public void Move()
-    //{
+    public void ForwardMove(InputAction.CallbackContext ctx)
+    {
+        ForwardMove(ctx.ReadValue<float>());
+    }
 
-    //}
+    public void ForwardMove(float dir)
+    {
+        moveForwardDirection = dir;
+    }
+
+    public void SideMove(InputAction.CallbackContext ctx)
+    {
+        SideMove(ctx.ReadValue<float>());
+    }
+
+    public void SideMove(float dir)
+    {
+        moveSideDirection = dir;
+    }
 
     //public void Move(InputAction.CallbackContext ctx)
     //{
