22d6d31001d925ebbdec03f48ac5fbba29172a7b
beanedhuman <zpguo2007@gmail.com>
2025-10-31 13:05:25 +0800

    Camera movement done
    Moving, rotate, and FOV zoom are done
    prob more ui next
    bye bye

--- Assets/Scripts/CameraSystem.cs
+++ Assets/Scripts/CameraSystem.cs
@@ -1,90 +1,125 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Unity.Cinemachine;
 
 public class CameraSystem : MonoBehaviour
 {
-    //private CameraSystem myControls;   //input system
-    //refs
     private CharacterController characterController;
+    public CinemachineCamera cinemachineCam;
     public GameObject pauseScreen;
 
-    public float moveSpeed = 10f;
-    public float speed = 5f;
-    //public float turnRate = 360f;
+    public float moveSpeed;
+    public float rotateSpeed;
+    [SerializeField] private float fovMax = 90f;
+    [SerializeField] private float fovMin = 10f;
 
     float moveForwardDirection = 0f;
     float moveSideDirection = 0f;
-
+    float cameraRotateDirection = 0f;
+    float cameraZoomDirection = 0f;
+    private float targetFieldOfView = 70f;
+    float edgeScrollSize = 20f;
+    [SerializeField]private bool useEdgeScroll = false;
     private void Awake()
     {
-        //For input system
-        //myControls = new CameraSystem();
-
         //refs
         characterController = GetComponent<CharacterController>();
     }
 
     void Update()
     {
-        //transform.position += transform.forward * moveDirection * speed * Time.deltaTime;
-        //transform.Rotate(0, turnRate * Time.deltaTime * turnDirection, 0);
+        HandleMovement();
+        if (useEdgeScroll) { HandleMovementEdgeScroll(); }
+        HandleRotation();
+        HandleZoom();
+    }
 
-        Vector3 moveDir = new Vector3(0, 0, 0);
+    private void HandleMovement()
+    {
+        //Vector for input direction
+        Vector3 inputDir = new Vector3(0, 0, 0);
 
-        if(moveForwardDirection > 0)
-        {
-            Debug.Log("mfd >0");
-            moveDir.z = +1f;
-        }
-        if (moveForwardDirection < 0)
-        {
-            Debug.Log("mfd <0");
-            moveDir.z = -1f;
-        }
-        if (moveForwardDirection == 0) { moveDir.z = 0f; }
+        //to change value into forwards/backwards/no frontal movement
+        if (moveForwardDirection > 0) { inputDir.z = +1f; }
+        if (moveForwardDirection < 0) { inputDir.z = -1f; }
+        if (moveForwardDirection == 0) { inputDir.z = 0f; }
+        //to change value into left/right/no side movement
+        if (moveSideDirection > 0) { inputDir.x = +1f; }
+        if (moveSideDirection < 0) { inputDir.x = -1f; }
+        if (moveSideDirection == 0) { inputDir.x = 0f; }
 
-        if (moveSideDirection > 0) { moveDir.x = +1f; }
-        if (moveSideDirection < 0) { moveDir.x = -1f; }
-        if (moveSideDirection == 0) { moveDir.x = 0f; }
+        //Vector for moving camera (mainly for if camera rotates, forwards is still forwards)
+        Vector3 moveDir = transform.forward * inputDir.z + transform.right * inputDir.x;
 
+        //Actually moving camera
         transform.position += moveDir * moveSpeed * Time.deltaTime;
     }
-    public void ForwardMove(InputAction.CallbackContext ctx)
+
+    private void HandleMovementEdgeScroll()
     {
-        ForwardMove(ctx.ReadValue<float>());
+        //Vector for input direction
+        Vector3 inputDir = new Vector3(0, 0, 0);
+
+        //detection of mouse position for edge scrolling
+        if (Input.mousePosition.x < edgeScrollSize) { inputDir.x = -1f; }
+        if (Input.mousePosition.x > Screen.width - edgeScrollSize) { inputDir.x = +1f; }
+        if (Input.mousePosition.y < edgeScrollSize) { inputDir.z = -1f; }
+        if (Input.mousePosition.y > Screen.height - edgeScrollSize) { inputDir.z = +1f; }
+
+        //Vector for moving camera (mainly for if camera rotates, forwards is still forwards)
+        Vector3 moveDir = transform.forward * inputDir.z + transform.right * inputDir.x;
+
+        //Actually moving camera
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
     }
 
-    public void ForwardMove(float dir)
+    private void HandleRotation()
     {
-        moveForwardDirection = dir;
+        //Value for rotation direction
+        float rotateDir = 0f;
+
+        //to change value into left/right/no side rotation
+        if (cameraRotateDirection > 0) { rotateDir = -1f; }
+        if (cameraRotateDirection < 0) { rotateDir = +1f; }
+        if (cameraRotateDirection == 0) { rotateDir = 0f; }
+
+        //Actually rotating camera
+        transform.eulerAngles += new Vector3(0, rotateDir * rotateSpeed * Time.deltaTime, 0);
     }
 
-    public void SideMove(InputAction.CallbackContext ctx)
+    private void HandleZoom()
     {
-        SideMove(ctx.ReadValue<float>());
+        if (cameraZoomDirection > 0 )
+        {
+            targetFieldOfView += 5f;
+        }
+        if (cameraZoomDirection < 0)
+        {
+            targetFieldOfView -= 5f;
+        }
+
+        float zoomSpeed = 10f;
+
+        targetFieldOfView = Mathf.Clamp(targetFieldOfView, fovMin, fovMax);
+
+        
+
+        cinemachineCam.Lens.FieldOfView = Mathf.Lerp(cinemachineCam.Lens.FieldOfView, targetFieldOfView, Time.deltaTime * zoomSpeed);
     }
 
-    public void SideMove(float dir)
-    {
-        moveSideDirection = dir;
-    }
+    //Function for gathering player input and changing it into value for if player is moving forwards or backwards
+    public void ForwardMove(InputAction.CallbackContext ctx) { ForwardMove(ctx.ReadValue<float>()); }
+    public void ForwardMove(float dir) { moveForwardDirection = dir; }
 
-    //public void Move(InputAction.CallbackContext ctx)
-    //{
-    //    Move(ctx.ReadValue<float>());
-    //}
+    //Function for gathering player input and changing it into value for if player is moving left or right
+    public void SideMove(InputAction.CallbackContext ctx) { SideMove(ctx.ReadValue<float>()); }
+    public void SideMove(float dir) { moveSideDirection = dir; }
 
-    //public void Move(float dir)
-    //{
-    //    moveDirection = dir;
-    //}
+    //Function for gathering player input and changing it into value for if player rotates left or right
+    public void RotateCamera(InputAction.CallbackContext ctx) { RotateCamera(ctx.ReadValue<float>()); }
+    public void RotateCamera(float dir) { cameraRotateDirection = dir; }
 
-    //public void Turn(float dir)
-    //{
-    //    turnDirection = dir;
-    //}
-    //public void Turn(InputAction.CallbackContext ctx)
-    //{
-    //    Turn(ctx.ReadValue<float>());
-    //}
+    //Function for zooming camera
+    public void ZoomCamera(InputAction.CallbackContext ctx) { ZoomCamera(ctx.ReadValue<float>()); }
+    public void ZoomCamera(float dir) { cameraZoomDirection = dir; }
 }
