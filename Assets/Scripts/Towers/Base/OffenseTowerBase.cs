f84fc52bdc36a1b7ed8bbd86f3ef976511ee2e0d
Shortturtle <67746558+Shortturtle@users.noreply.github.com>
2025-10-31 18:13:44 +0800

    updated Offense Tower code

--- Assets/Scripts/Towers/Base/OffenseTowerBase.cs
+++ Assets/Scripts/Towers/Base/OffenseTowerBase.cs
@@ -1,23 +1,25 @@
+using System;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 [RequireComponent (typeof(SphereCollider))]
-public class OffenseTowerbase : BaseTowerClass
+public class OffenseTowerBase : BaseTowerClass
 {
     // Range variables
     protected float rangeValue;
     protected SphereCollider rangeSphere;
 
     // Target variables
-    protected List<GameObject> targets = new List<GameObject>();
+    protected List<BaseEnemy> targets = new List<BaseEnemy>();
     protected GameObject currentTarget;
 
     protected enum TargettingModes
     {
         First, Last, Close, Strong
     }
-    protected TargettingModes targettingMode;
+    protected TargettingModes targettingMode = TargettingModes.First;
     protected int targetModeNum = 0;
 
     protected enum ChangeDirection
@@ -34,7 +36,7 @@ public class OffenseTowerbase : BaseTowerClass
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log((TargettingModes)Enum.ToObject(typeof(TargettingModes), targetModeNum));
     }
 
     // Update is called once per frame
@@ -47,15 +49,15 @@ public class OffenseTowerbase : BaseTowerClass
     {
         if (other.gameObject.CompareTag("Enemy") || other.gameObject.GetComponent<BaseEnemy>() != null)
         {
-            targets.Add(other.gameObject);
+            targets.Add(other.GetComponent<BaseEnemy>());
         }
     }
 
     protected void OnTriggerExit(Collider other)
     {
-        if (targets.Contains(other.gameObject))
+        if (targets.Contains(other.GetComponent<BaseEnemy>()))
         {
-            targets.Remove(other.gameObject);
+            targets.Remove(other.GetComponent<BaseEnemy>());
         }
     }
 
@@ -65,12 +67,46 @@ public class OffenseTowerbase : BaseTowerClass
 
         if (targetModeNum < 0)
         {
-            
+            targetModeNum = Enum.GetNames(typeof(TargettingModes)).Length - 1;
         }
+
+        else if (targetModeNum > (Enum.GetNames(typeof(TargettingModes)).Length - 1))
+        {
+            targetModeNum = 0;
+        }
+
+        targettingMode = (TargettingModes)Enum.ToObject(typeof(TargettingModes), targetModeNum);
+
     }
 
-    protected void GetTargetEnemy()
+    protected GameObject GetTargetEnemy()
     {
+        switch (targettingMode)
+        {
+            case TargettingModes.First:
+                BaseEnemy FirstInLine = null;
+                float highestPercentage = 0;
+                foreach(BaseEnemy option in targets)
+                {
+                    if (option.percentageDistance > highestPercentage)
+                    {
+                        FirstInLine = option;
+                        highestPercentage = option.percentageDistance;
+                    }
+                }
+                return FirstInLine.gameObject;
 
+            case TargettingModes.Last:
+
+                break;
+
+            case TargettingModes.Close:
+
+                break;
+
+            case TargettingModes.Strong:
+
+                break;
+        }
     }
 }
