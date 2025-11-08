103acdafb87bf2599846b6c5a4c074a2066ce4c5
beanedhuman <zpguo2007@gmail.com>
2025-11-08 23:20:56 +0800

    Finished minigame 2
    poorly
    i suck lmao

--- Assets/Scripts/Minigame/FruitSpawner.cs
+++ Assets/Scripts/Minigame/FruitSpawner.cs
@@ -7,6 +7,8 @@ public class FruitSpawner : MonoBehaviour
 
     public Transform parentGameObject;
     public GameObject[] fruitPrefabs;
+    //public GameObject spawner;
+    //public float locationX;
 
     public float minSpawnDelay = 0.25f;
     public float maxSpawnDelay = 1f;
@@ -25,11 +27,12 @@ public class FruitSpawner : MonoBehaviour
         yield return new WaitForSeconds(1f);
         while (enabled)
         {
-            GameObject prefab = fruitPrefabs[Random.Range(0, fruitPrefabs.Length)];
-            
+            //locationX = Random.Range(-400, 400);
+            //spawner.transform.position = new Vector3(locationX,0,0);
+            GameObject prefab = fruitPrefabs[Random.Range(0, fruitPrefabs.Length)];            
 
-            GameObject enemy = Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
-            enemy.transform.SetParent(GameObject.FindGameObjectWithTag("MinigameFN").transform, false);
+            GameObject fruit = Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+            fruit.transform.SetParent(GameObject.FindGameObjectWithTag("MinigameFN").transform, false);
 
             yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
         }
