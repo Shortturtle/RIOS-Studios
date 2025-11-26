56c90b963079a1b78fa114423f73bdaa69797d54
Shortturtle <67746558+Shortturtle@users.noreply.github.com>
2025-11-26 14:53:52 +0800

    whoops forgot to save the script

--- Assets/Scripts/GameManagers/EnemyWaveManager.cs
+++ Assets/Scripts/GameManagers/EnemyWaveManager.cs
@@ -1,11 +1,29 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyWaveManager : MonoBehaviour
 {
+    static EnemyWaveManager instance;
+
+    public EnemyWaves waveHolder;
+
+    public int currentWave;
+    public int maxWaves;
+
+    private List<GameObject> enemiesInCurrentWave = new List<GameObject>();
+
+    public float TimeBetweenSpawns;
+
+    private void Awake()
+    {
+        maxWaves = waveHolder.enemyWaves.Count;
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        currentWave = 0;
+        enemiesInCurrentWave = waveHolder.enemyWaves[currentWave - 1].wave;
     }
 
     // Update is called once per frame
