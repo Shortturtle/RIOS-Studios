6e718fa4b8a724eba2b0bc59455cc6f8b9d6286d
beanedhuman <zpguo2007@gmail.com>
2025-12-01 00:15:48 +0800

    Worked on ice minigame
    very basic functionality

---
+++ Assets/Scripts/Minigame/IceMinigame/MinigameFire.cs
@@ -0,0 +1,19 @@
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class MinigameFire : MonoBehaviour
+{
+    public GameObject minigameFire;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        minigameFire.transform.position = Input.mousePosition;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(other.gameObject);
+    }
+}
