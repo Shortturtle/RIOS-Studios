9dea61c06a1bf284e9a6d2e4b5857a9297748fdc
beanedhuman <zpguo2007@gmail.com>
2025-12-06 19:08:43 +0800

    Created quest item prefab
    To use it, just drag it into the scene and place the fbx or whatever underneath it

--- Assets/Resources/Quests/Tutorial Quests/TutorialQuestStep.cs
+++ Assets/Resources/Quests/Tutorial Quests/TutorialQuestStep.cs
@@ -1,16 +1,56 @@
 using UnityEngine;
 
-public class TutorialQuestStep : MonoBehaviour
+public class TutorialQuestStep : QuestStep
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    //numbers for quest functionality
+    private int numberOfItemsCollected = 0;
+    private int numberOfItemsToComplete = 3;
+
+
+    private void Start()
     {
-        
+        //IMPORTANT FOR QUEST LOG
+        UpdateState();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        GameEventManager.instance.miscEvents.onCollectKanade += TestActionDone;
+    }
+    private void OnDisable()
+    {
+        GameEventManager.instance.miscEvents.onCollectKanade -= TestActionDone;
+    }
+
+    //record of the number of kanades collected to progress the quest
+    private void TestActionDone()
+    {
+        if (numberOfItemsCollected < numberOfItemsToComplete)
+        {
+            numberOfItemsCollected++;
+            UpdateState();
+        }
+
+        if (numberOfItemsCollected >= numberOfItemsToComplete)
+        {
+            FinishedQuestStep();
+        }
+    }
+
+    //call to update the state of the quest
+    private void UpdateState()
+    {
+        //idk what this
+        string state = numberOfItemsCollected.ToString();
+        //shows text in the qLog UI
+        string status = "Collected " + numberOfItemsCollected + " / " + numberOfItemsToComplete + " Items.";
+        ChangeState(state, status);
+    }
+
+    //i have no clue what this is either fuccc
+    protected override void SetQuestStepState(string state)
+    {
+        this.numberOfItemsCollected = System.Int32.Parse(state);
+        UpdateState();
     }
 }
