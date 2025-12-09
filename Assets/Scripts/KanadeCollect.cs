72e00ae36bdab6bf8a253f846a1d705723331b53
beanedhuman <zpguo2007@gmail.com>
2025-12-09 12:22:43 +0800

    working
    ...

--- Assets/Scripts/KanadeCollect.cs
+++ Assets/Scripts/KanadeCollect.cs
@@ -1,12 +1,16 @@
+using System.Collections;
 using UnityEngine;
 
 public class KanadeCollect : MonoBehaviour
 {
+    public GameObject objectRenderer;
+    private bool collectable = true;
     //test for the quest functionality
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && collectable == true)
         {
+            collectable = false;
             CollectKanade();
         }
     }
@@ -16,6 +20,15 @@ public class KanadeCollect : MonoBehaviour
         //ref to the action in miscEvents script through GameEventManager (i assume)
         GameEventManager.instance.miscEvents.KanadeCollected();
 
-        Destroy(gameObject);
+        StartCoroutine(AfterCollected());
+    }
+
+    private IEnumerator AfterCollected()
+    {
+        objectRenderer.SetActive(false);
+
+        yield return new WaitForSeconds(5);
+        collectable = true;
+        objectRenderer.SetActive(true);
     }
 }
