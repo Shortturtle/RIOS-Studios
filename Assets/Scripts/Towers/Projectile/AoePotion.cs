c1562bfe84d719368ac050ceb34565247847626e
Esquire5100 <KLEAH.24@ichat.sp.edu.sg>
2026-01-07 14:09:54 +0800

    Created a potion projectile prefab but the lobbing movement is still wip

--- Assets/Scripts/Towers/Projectile/AoePotion.cs
+++ Assets/Scripts/Towers/Projectile/AoePotion.cs
@@ -1,16 +1,50 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class AoEPotion : BaseProjectileClass
 {
+    public Transform pivot, model;
+    public float AoERange;
     public GameObject potion;
 
-    protected override void ProjectileEffect()
+    private void Start()
     {
-        BaseEnemyClass frickThisGuy = target.GetComponent<BaseEnemyClass>();
+        Vector3 startPosition = transform.position;
 
-        if (frickThisGuy != null)
+        Vector3 centerPosition = (transform.position + targetPosition) * 2f;
+        transform.position = centerPosition;
+
+        transform.forward = targetPosition - transform.position;
+
+        model.transform.position = startPosition;
+    }
+
+    protected override void ToTarget() //put the lob
+    {
+        pivot.localRotation = Quaternion.RotateTowards(pivot.localRotation, Quaternion.Euler(0f, 0f, 180f), speed * Time.deltaTime);
+        model.rotation = Quaternion.identity;
+
+        if (Vector3.Distance(model.transform.position, targetPosition) <= 0.3f)
         {
-            frickThisGuy.Damage(damage);
+            ProjectileEffect();
         }
+    }
+
+    protected override void ProjectileEffect() //aoe
+    {
+        Collider[] collidersInRange = Physics.OverlapSphere(model.transform.position, AoERange);
+
+        foreach (Collider col in collidersInRange)
+        {
+            BaseEnemyClass frickThisGuy = col.GetComponent<BaseEnemyClass>();
+
+            if (frickThisGuy != null)
+            {
+                projectileEvent.Post(this.gameObject);
+                frickThisGuy.Damage(damage);
+            }
+        }
+
+        Destroy(gameObject);
     }
 }
