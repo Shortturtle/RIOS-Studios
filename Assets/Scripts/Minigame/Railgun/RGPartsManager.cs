f2c94666bbd74cd12fcea3bdeaf04a3090232dd9
beanedhuman <zpguo2007@gmail.com>
2026-01-09 19:12:03 +0800

    Starting on new minigame
    for railgunner

---
+++ Assets/Scripts/Minigame/Railgun/RGPartsManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class RGPartsManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
