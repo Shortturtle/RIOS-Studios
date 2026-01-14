5f21b151bd90ca1ac23a68078f977c7dcfc9d88a
KLeah <KLEAH.24@ichat.sp.edu.sg>
2026-01-14 13:11:42 +0800

    Idk if axe swing works icl

--- Assets/Scripts/Towers/Projectile/AxeSwing.cs
+++ Assets/Scripts/Towers/Projectile/AxeSwing.cs
@@ -2,9 +2,8 @@ using UnityEngine;
 
 public class AxeSwing : BaseProjectileClass
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    protected override void ToTarget()
     {
-        
+        base.ToTarget();
     }
 }
