212483f44e868599c803335543d3de593a81d024
KLeah <KLEAH.24@ichat.sp.edu.sg>
2026-01-14 13:59:50 +0800

    Axe Swing works

--- Assets/Scripts/Towers/Projectile/AxeSwing.cs
+++ Assets/Scripts/Towers/Projectile/AxeSwing.cs
@@ -1,9 +1,49 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AxeSwing : BaseProjectileClass
 {
+    public float AttackRadius;
+    protected override void Update()
+    {
+        if (target == null)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public override void InitializeProjectile(float projectileDamage, GameObject projectileTarget, Vector3 projectileTargetPosition)
+    {
+        base.InitializeProjectile(projectileDamage, projectileTarget, projectileTargetPosition);
+        ProjectileEffect();
+    }
+
+    protected override void ProjectileEffect()
+    {
+        Collider[] collidersInRange = Physics.OverlapSphere(transform.position, AttackRadius);
+
+        foreach (Collider col in collidersInRange)
+        {
+            BaseEnemyClass frickThisGuy = col.GetComponent<BaseEnemyClass>();
+
+            if (frickThisGuy != null)
+            {
+                projectileEvent.Post(this.gameObject);
+                frickThisGuy.Damage(damage);
+            }
+        }
+
+        Destroy(gameObject);
+    }
+
     protected override void ToTarget()
     {
-        base.ToTarget();
+        // AxeSwing does not need to move towards a target
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, AttackRadius);
     }
 }
