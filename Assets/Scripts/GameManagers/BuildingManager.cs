c3d710c297e901d0af90dbc54461f6472a1fa822
Shortturtle <67746558+Shortturtle@users.noreply.github.com>
2026-01-28 03:12:24 +0800

    BETA!!!!

--- Assets/Scripts/GameManagers/BuildingManager.cs
+++ Assets/Scripts/GameManagers/BuildingManager.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using UnityEditor.PackageManager;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
--- Assets/Scripts/GameManagers/EnemyWaveManager.cs
+++ Assets/Scripts/GameManagers/EnemyWaveManager.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEngine.InputManagerEntry;
 
 public class EnemyWaveManager : MonoBehaviour
 {
