22b6a4d2e98e6edae3637f997e5d721008d2a623
beanedhuman <zpguo2007@gmail.com>
2026-02-01 20:48:12 +0800

    Edited main berry
    can now spawn diff sub berries

--- Assets/Scripts/Enemy/Scripts/FantasyEnemies/MainBerryEnemy.cs
+++ Assets/Scripts/Enemy/Scripts/FantasyEnemies/MainBerryEnemy.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class MainBerryEnemy : BaseEnemyClass
 {
     //the sub berry prefab, and no. of berries that will spawn
-    public GameObject subBerry;
+    public GameObject subBerry1;
+    public GameObject subBerry2;
+    public GameObject subBerry3;
     public int berrySpawns;
 
     //on death, spawn sub berries
@@ -16,14 +18,25 @@ public class MainBerryEnemy : BaseEnemyClass
         dirToNextWaypoint.Normalize();
         float splitNumber = -1f;
 
-        //for every berry spawns left, spawns a sub berry with a gap between each other
-        while (berrySpawns > 0)
-        {
-            splitNumber += 0.75f;
-            GameObject tempEnemy = Instantiate(subBerry, transform.position + (dirToNextWaypoint *( speed * splitNumber)), Quaternion.identity);
-            tempEnemy.GetComponent<BaseEnemyClass>().InitializeEnemy_OnTrack(waypointManager,waypointIndex, distanceTravelled + (speed * splitNumber));
-            berrySpawns--;
-        }
+
+        splitNumber += 0.75f;
+        GameObject tempEnemy1 = Instantiate(subBerry1, transform.position + (dirToNextWaypoint * (speed * splitNumber)), Quaternion.identity);
+        tempEnemy1.GetComponent<BaseEnemyClass>().InitializeEnemy_OnTrack(waypointManager, waypointIndex, distanceTravelled + (speed * splitNumber));
+        splitNumber += 0.75f;
+        GameObject tempEnemy2 = Instantiate(subBerry2, transform.position + (dirToNextWaypoint * (speed * splitNumber)), Quaternion.identity);
+        tempEnemy2.GetComponent<BaseEnemyClass>().InitializeEnemy_OnTrack(waypointManager, waypointIndex, distanceTravelled + (speed * splitNumber));
+        splitNumber += 0.75f;
+        GameObject tempEnemy3 = Instantiate(subBerry3, transform.position + (dirToNextWaypoint * (speed * splitNumber)), Quaternion.identity);
+        tempEnemy3.GetComponent<BaseEnemyClass>().InitializeEnemy_OnTrack(waypointManager, waypointIndex, distanceTravelled + (speed * splitNumber));
+
+        ////for every berry spawns left, spawns a sub berry with a gap between each other
+        //while (berrySpawns > 0)
+        //{
+        //    splitNumber += 0.75f;
+        //    GameObject tempEnemy = Instantiate(subBerry, transform.position + (dirToNextWaypoint *( speed * splitNumber)), Quaternion.identity);
+        //    tempEnemy.GetComponent<BaseEnemyClass>().InitializeEnemy_OnTrack(waypointManager,waypointIndex, distanceTravelled + (speed * splitNumber));
+        //    berrySpawns--;
+        //}
 
         base.Die();
     }
