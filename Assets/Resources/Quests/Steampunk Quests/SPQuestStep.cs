339f5ade8a984600e5997fd4cd149d6a942c1444
beanedhuman <zpguo2007@gmail.com>
2026-02-08 16:18:11 +0800

    Placed quests in medieval lobby
    idk if works, still gotta edit it more too other than fixes

---
+++ Assets/Resources/Quests/Steampunk Quests/SPQuestStep.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class SPQuestStep : QuestStep
+{
+    //numbers for quest functionality
+    private int numberOfMushroomsKilled = 0;
+    private int numberOfMushroomsToKill = 10;
+
+
+    private void Start()
+    {
+        ////IMPORTANT FOR QUEST LOG
+        //UpdateState();
+    }
+
+    private void OnEnable()
+    {
+        //GameEventManager.instance.killEvents.onKillMushroom += FruitKillActionDone;
+    }
+    private void OnDisable()
+    {
+        //GameEventManager.instance.killEvents.onKillMushroom -= FruitKillActionDone;
+    }
+
+    //record of the number of kanades collected to progress the quest
+    private void FruitKillActionDone()
+    {
+        if (numberOfMushroomsKilled < numberOfMushroomsToKill)
+        {
+            numberOfMushroomsKilled++;
+            //UpdateState();
+        }
+
+        if (numberOfMushroomsKilled >= numberOfMushroomsToKill)
+        {
+            FinishedQuestStep();
+        }
+    }
+
+    //call to update the state of the quest
+    //private void UpdateState()
+    //{
+    //    //idk what this
+    //    string state = numberOfMushroomsKilled.ToString();
+    //    //shows text in the qLog UI
+    //    string status = "Killed " + numberOfMushroomsKilled + " / " + numberOfMushroomsToKill + " Enemies.";
+    //    ChangeState(state, status);
+    //}
+
+    protected override void SetQuestStepState(string state)
+    {
+        this.numberOfMushroomsKilled = System.Int32.Parse(state);
+        //UpdateState();
+    }
+}
