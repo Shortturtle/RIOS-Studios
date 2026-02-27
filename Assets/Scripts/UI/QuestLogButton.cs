04671fd4a91627d48b7c703669ef72c6ff6173aa
beanedhuman <zpguo2007@gmail.com>
2026-02-27 22:32:27 +0800

    Scifi quest tested
    one more test to go

--- Assets/Scripts/UI/QuestLogButton.cs
+++ Assets/Scripts/UI/QuestLogButton.cs
@@ -47,10 +47,10 @@ public class QuestLogButton : MonoBehaviour, ISelectHandler
                 buttonText.color = Color.red;
                 break;
             case QuestState.CAN_FINISH:
-                buttonText.color = new Color(0,0,255,1);
+                buttonText.color = new Color32(0,0,255,255);
                 break;
             case QuestState.FINISHED:
-                buttonText.color = new Color(50f, 180f, 0f, 1f);
+                buttonText.color = new Color32(50, 180, 0, 255);
                 break;
             default:
                 Debug.LogWarning("Quest State not recognised by switch statement: " + state);
